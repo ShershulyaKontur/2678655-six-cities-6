43e6140af1a83a52056955cae625161f9a333965
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-03-25 16:59:45 +0700

    feat: вынес сортировку в отдельный компонент

---
+++ src/components/sorting/sorting.tsx
@@ -0,0 +1,37 @@
+import cn from 'classnames';
+
+export type SortType = 'popular' | 'priceLowToHigh' | 'priceHighToLow' | 'topRated';
+
+const SORT_OPTIONS: { type: SortType; label: string }[] = [
+  { type: 'popular', label: 'Popular' },
+  { type: 'priceLowToHigh', label: 'Price: low to high' },
+  { type: 'priceHighToLow', label: 'Price: high to low' },
+  { type: 'topRated', label: 'Top rated first' },
+];
+
+export function Sorting(): JSX.Element {
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-type" tabIndex={0}>
+        Popular
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use xlinkHref="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul className="places__options places__options--custom places__options--opened">
+        {SORT_OPTIONS.map(({ type, label }) => (
+          <li
+            key={type}
+            className={cn('places__option',
+              'places__option--active'
+            )}
+            tabIndex={0}
+          >
+            {label}
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+}
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -6,6 +6,7 @@ import { LocationsList } from '../../ui/locations-list/locations-list';
 import { Heading } from '../../ui/heading/heading';
 import { useSelector } from 'react-redux';
 import { getCity, getOffers } from '../../store/selectors';
+import { Sorting } from '../../components/sorting/sorting';
 
 
 export function MainPage(): JSX.Element{
@@ -29,21 +30,7 @@ export function MainPage(): JSX.Element{
             <section className="cities__places places">
               <Heading tag='h2' className="visually-hidden">Places</Heading>
               <b className="places__found">{offersByCity.length} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active"tabIndex={0}>Popular</li>
-                  <li className="places__option" tabIndex={0}>Price: low to high</li>
-                  <li className="places__option" tabIndex={0}>Price: high to low</li>
-                  <li className="places__option" tabIndex={0}>Top rated first</li>
-                </ul>
-              </form>
+              <Sorting />
               <OffersList offers={offersByCity} setChosenId={setChosenId} />
             </section>
             <div className="cities__right-section">
