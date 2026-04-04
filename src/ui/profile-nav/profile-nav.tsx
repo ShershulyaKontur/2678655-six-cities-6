dc1a17c8435fe26e88d4541bd3be8efbf9b5b832
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-04-04 22:01:52 +0700

    feat: доработал шапку страницу

--- src/ui/profile-nav/profile-nav.tsx
+++ src/ui/profile-nav/profile-nav.tsx
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const/const';
+import { AppRoute, AuthorizationStatus } from '../../const/const';
+import { AuthStatus } from '../../types';
 
-export function ProfileNav(){
-  return(
+export function ProfileNav({ authorizationStatus }: { authorizationStatus: AuthStatus }): JSX.Element {
+  return (
     <nav className="header__nav">
       <ul className="header__nav-list">
-        <li className="header__nav-item user">
-          <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
-            <div className="header__avatar-wrapper user__avatar-wrapper" />
-            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-            <span className="header__favorite-count">3</span>
-          </Link>
-        </li>
-        <li className="header__nav-item">
-          <Link className="header__nav-link" to={AppRoute.Login}>
-            <span className="header__signout">Sign out</span>
-          </Link>
-        </li>
+        {authorizationStatus === AuthorizationStatus.Auth ? (
+          <>
+            <li className="header__nav-item user">
+              <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
+                <div className="header__avatar-wrapper user__avatar-wrapper" />
+                <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                <span className="header__favorite-count">3</span>
+              </Link>
+            </li>
+            <li className="header__nav-item">
+              <Link className="header__nav-link" to={AppRoute.Main}>
+                <span className="header__signout">Sign out</span>
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="header__nav-item user">
+            <a className="header__nav-link header__nav-link--profile" href="#">
+              <div className="header__avatar-wrapper user__avatar-wrapper" />
+              <span className="header__login">Sign in</span>
+            </a>
+          </li>
+        )}
       </ul>
     </nav>
   );
