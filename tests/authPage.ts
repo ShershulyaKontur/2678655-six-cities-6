d389e6b2e6566684c895dda382cc86cc77db3fba
Egor Osipchuk <kaban.kabanytch@yandex.ru>
2026-04-11 23:03:54 +0700

    fix:213

--- tests/authPage.ts
+++ tests/authPage.ts
@@ -9,6 +9,7 @@ export class AuthPage {
 
   constructor(page: Page, url = 'http://localhost:5173/login') {
     this.url = url;
+
     this.page = page;
     this.usernameInput = page.locator('input[name="email"]');
     this.passwordInput = page.locator('input[name="password"]');
